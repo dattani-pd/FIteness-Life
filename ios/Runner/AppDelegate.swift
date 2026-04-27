5c54e2b2987a6d58555de5e6e04bae6f221502db
dattani-pd <dattanipd@gmail.com>
2026-04-27 12:42:12 +0530

    Harden iOS root window background initialization

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -14,6 +14,26 @@ import UIKit
         flutterEngine.run()
         GeneratedPluginRegistrant.register(with: flutterEngine)
 
+        if #available(iOS 13.0, *) {
+            // iOS 13+ uses SceneDelegate for window setup.
+        } else {
+            let controller = FlutterViewController(
+                engine: flutterEngine,
+                nibName: nil,
+                bundle: nil
+            )
+            controller.view.backgroundColor = .white
+            controller.view.isOpaque = true
+            controller.overrideUserInterfaceStyle = .light
+
+            let window = UIWindow(frame: UIScreen.main.bounds)
+            window.rootViewController = controller
+            window.backgroundColor = .white
+            window.isOpaque = true
+            window.makeKeyAndVisible()
+            self.window = window
+        }
+
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 }
